2c12d9dc715d09fea84dbd1c5ed95d0cf9162bc5
Boxer300 <romano2789@nycstudents.net>
2026-03-16 12:32:13 -0400

    Update App.vue

--- vue-data/src/App.vue
+++ vue-data/src/App.vue
@@ -5,7 +5,20 @@
 </template>
 
 <script setup>
-
+import { ref, onMounted }
+const arrest ref({})
+async function getArrest(){
+  try {
+    const response = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
+    const data = await response.json()
+    arrest.value = data
+  } catch (error) {
+    console.log(error)
+  }
+}
+onMounted(() => {
+  getArrest()
+});
 </script>
 
 <style  scoped>
